f5f524d44d368529b3ecfad6237ad1dd4dd698d3
Jaime Andres <jaime.artunduaga@uao.edu.co>
2021-12-10 13:50:25 -0500

    Mercado Liebre firstCommit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
+app.get('/registro', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
+
+app.listen(3000, () => console.log("Servidor corriendo en Puerto: 3000"));
+
